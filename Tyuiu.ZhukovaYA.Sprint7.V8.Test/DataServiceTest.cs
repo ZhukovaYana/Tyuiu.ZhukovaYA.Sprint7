a5968d1d8aafb6ae67ce392c4a9ddd92d426c288
Yana <yana_zhukova2004@mail.ru>
2024-12-27 17:34:05 +0500

    Test

---
+++ Tyuiu.ZhukovaYA.Sprint7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Tyuiu.ZhukovaYA.Sprint7.V8.Lib;
+
+namespace Tyuiu.ZhukovaYA.Sprint7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestFuelConsumptionPerDistanceTraveled()
+        {
+            Actions action = new Actions();
+            Cars car = new Cars()
+            {
+                ID = 1,
+                LicensePlate = "1",
+                CarBrand = "1",
+                TechnicalCondition = true,
+                AverageSpeed = 1,
+                LoadCapacity = 1,
+                FuelConsumption = 20
+            };
+            double result = action.FuelConsumptionPerDistanceTraveled(car,200);
+            Assert.AreEqual(result, 40);
+        }
+
+        [TestMethod]
+        public void TestCheckingTheCar()
+        {
+            Actions action = new Actions();
+            List<Cars> cars = new List<Cars>();
+            cars.Add(new Cars
+            {
+                ID = 1,
+                LicensePlate = "1",
+                CarBrand = "1",
+                TechnicalCondition = true,
+                AverageSpeed = 1,
+                LoadCapacity = 1,
+                FuelConsumption = 20
+            });
+            cars.Add(new Cars
+            {
+                ID = 1,
+                LicensePlate = "1",
+                CarBrand = "1",
+                TechnicalCondition = false,
+                AverageSpeed = 1,
+                LoadCapacity = 200,
+                FuelConsumption = 20
+            });
+
+            List<Cars> result = action.CheckingTheCar(cars, 200);
+
+            List<Cars> carsResult = new List<Cars>();
+            CollectionAssert.AreEqual(result, carsResult);
+        }
+    }
+}
---
+++ Tyuiu.ZhukovaYA.Sprint7.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhukovaYA.Sprint7.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhukovaYA.Sprint7.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f81a63d-41a2-4c98-a48c-6e95dc90ea44")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
